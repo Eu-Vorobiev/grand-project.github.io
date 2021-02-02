b1d9ce977b6a2c42301bb90131b68a81165aeeaa
Eugene <dijjekas06@gmail.com>
2021-02-02 13:33:02 +0200

    Responive till 576px w/out slider

--- js/main-page.js
+++ js/main-page.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+  
   let accordHeader = document.querySelectorAll(".accordion__header");
 
   /* Accordion */
@@ -8,6 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
       this.classList.toggle("accordion__header--active");
       this.parentElement.classList.toggle("accordion__item--show");
     });
-  });
+    /* для активного проверять HAsClass? */
+  });  
 
 });
+
+// $(document).ready(function () {
+//   $(".slider").slick({
+//     infinite: true,
+//     slidesToShow: 1,
+//     sliderToScroll: 1,
+//     arrows: false,
+//     dots: true
+//   });
+// });
