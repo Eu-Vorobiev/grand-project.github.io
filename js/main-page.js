1770f23b6c90a43127f74a789d38451d18014800
Eugene <dijjekas06@gmail.com>
2021-02-04 14:21:06 +0200

    Full responsive, burger menu, animation&scroll out

--- js/main-page.js
+++ js/main-page.js
@@ -1,16 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  /* Scroll Out */
+  ScrollOut({
+    once: true
+  });
   
+  /* Accordion */
   let accordHeader = document.querySelectorAll(".accordion__header");
 
-  /* Accordion */
   accordHeader.forEach( function (elem) {
     elem.addEventListener("click", function () {
       this.classList.toggle("accordion__header--active");
       this.parentElement.classList.toggle("accordion__item--show");
     });
-    /* для активного проверять HAsClass? */
   });  
 
+  /* Burger Menu */
+  let burger = document.querySelector(".burger");
+  let burgerMenu = document.querySelector(".burger__menu");
+  let btnClose = document.querySelector(".close");
+
+  burger.addEventListener("click", function () {
+    burgerMenu.classList.add("burger__menu--active");
+  });
+  btnClose.addEventListener("click", () => {
+    burgerMenu.classList.remove("burger__menu--active");
+  });
 });
 
 // $(document).ready(function () {
