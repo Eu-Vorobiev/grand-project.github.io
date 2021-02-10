bb5d5eefa6cf8d46e172131f12889750dc712cd3
Eugene <dijjekas06@gmail.com>
2021-02-10 19:54:58 +0200

    Added slider, there are some bugs

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".slider__wrap").slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    initialSlide: 2,
+  });
+  
+});
