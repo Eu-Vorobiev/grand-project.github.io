d1434519b131a97fbde90dcc372b8dafba6bf29e
Eugene <dijjekas06@gmail.com>
2021-02-15 15:18:00 +0200

    Added slider with responsive

--- js/slider.js
+++ js/slider.js
@@ -6,6 +6,16 @@ $(document).ready(function () {
     adaptiveHeight: true,
     slidesToShow: 2,
     initialSlide: 2,
+    responsive: [
+      {
+        breakpoint: 930,
+        settings: {
+          centerMode: true,
+          centerPadding: '10px',
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
-  
 });
