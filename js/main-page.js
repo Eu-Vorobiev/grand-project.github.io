e6507c2897a478a5eee374b9d8665ddaccf71a18
Eugene <dijjekas06@gmail.com>
2021-02-18 11:59:38 +0200

    Feedback form w\out validation

--- js/main-page.js
+++ js/main-page.js
@@ -61,13 +61,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 });
-
-// $(document).ready(function () {
-//   $(".slider").slick({
-//     infinite: true,
-//     slidesToShow: 1,
-//     sliderToScroll: 1,
-//     arrows: false,
-//     dots: true
-//   });
-// });
